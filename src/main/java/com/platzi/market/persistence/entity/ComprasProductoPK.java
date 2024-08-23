ec545e034ca9d90594af36a0105d1a990a9e5ed6
Carlos Hernandez <carlos.hernandez@validatedid.com>
2024-08-22 22:19:56 -0400

    feat: create endpoint for purchases operations

--- src/main/java/com/platzi/market/persistence/entity/ComprasProductoPK.java
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProductoPK.java
@@ -3,20 +3,22 @@ package com.platzi.market.persistence.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
+import java.math.BigInteger;
+
 @Embeddable
 public class ComprasProductoPK {
 
     @Column(name = "id_compra")
-    private Integer idCompra;
+    private BigInteger idCompra;
 
     @Column(name = "id_producto")
     private Integer idProducto;
 
-    public Integer getIdCompra() {
+    public BigInteger getIdCompra() {
         return idCompra;
     }
 
-    public void setIdCompra(Integer idCompra) {
+    public void setIdCompra(BigInteger idCompra) {
         this.idCompra = idCompra;
     }
 
