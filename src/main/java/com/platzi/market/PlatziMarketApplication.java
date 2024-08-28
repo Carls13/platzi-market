446736e79f1cf258ea78651919766bc6aef6fdd6
Carlos Hernandez <carlos.hernandez@validatedid.com>
2024-08-27 20:56:54 -0400

    config: EnableAutoConfiguration

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -1,9 +1,12 @@
 package com.platzi.market;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class PlatziMarketApplication {
 
 	public static void main(String[] args) {
